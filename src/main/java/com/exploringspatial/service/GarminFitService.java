58264500f0bf1b4854b2d7ac59eb34d2ea977b50
Steve Mitchell <stevecmitchell@gmail.com>
2015-01-04 09:51:59 -0600

    Added bounding box to GeoJSON

--- src/main/java/com/exploringspatial/service/GarminFitService.java
+++ src/main/java/com/exploringspatial/service/GarminFitService.java
@@ -4,8 +4,6 @@ import com.exploringspatial.domain.FitActivity;
 import com.exploringspatial.fit.GarminFitListener;
 import com.garmin.fit.*;
 import com.vividsolutions.jts.geom.*;
-import com.vividsolutions.jts.io.ParseException;
-import com.vividsolutions.jts.io.WKTReader;
 import com.vividsolutions.jts.simplify.DouglasPeuckerSimplifier;
 import org.apache.log4j.Logger;
 import org.geotools.feature.simple.SimpleFeatureBuilder;
@@ -13,18 +11,15 @@ import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
 import org.geotools.geojson.feature.FeatureJSON;
 import org.geotools.geojson.geom.GeometryJSON;
 import org.geotools.geometry.jts.JTSFactoryFinder;
-import org.geotools.referencing.CRS;
 import org.geotools.referencing.crs.DefaultGeographicCRS;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.geometry.BoundingBox;
 import org.opengis.referencing.FactoryException;
-import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.List;
 
 /**
  * The purpose of this class is to turn a Garmin FIT file into GeoJSON for
@@ -34,9 +29,12 @@ import java.util.List;
  */
 public class GarminFitService {
     private final Logger log = Logger.getLogger(GarminFitService.class);
+    private final GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory();
+
     /**
      * The purpose of this method is to read a Garmin FIT file from an
      * input stream an extract the activity data.
+     *
      * @param in - InputStream from a Garmin FIT file.
      * @return FitActivity - date from FIT file.
      */
@@ -44,8 +42,8 @@ public class GarminFitService {
         final GarminFitListener listener = new GarminFitListener(new FitActivity());
         final Decode decode = new Decode();
         final MesgBroadcaster mesgBroadcaster = new MesgBroadcaster(decode);
-        mesgBroadcaster.addListener((RecordMesgListener)listener);
-        mesgBroadcaster.addListener((SessionMesgListener)listener);
+        mesgBroadcaster.addListener((RecordMesgListener) listener);
+        mesgBroadcaster.addListener((SessionMesgListener) listener);
         try {
             mesgBroadcaster.run(in);
         } catch (FitRuntimeException e) {
@@ -73,11 +71,12 @@ public class GarminFitService {
      * The purpose of this method is to use the
      * Douglas-Peuker line simplification algorithm to reduce the
      * number of points in the polyline while maintaining route fidelity.
+     *
      * @param polyline - The full set of points from the device.
      * @return Simplified Geometry.
      */
     public Geometry simplifyLineString(final Coordinate[] polyline) {
-        final GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory();
+
         final LineString lineString = geometryFactory.createLineString(polyline);
         return DouglasPeuckerSimplifier.simplify(lineString, 0.00001);
     }
@@ -98,32 +97,58 @@ public class GarminFitService {
         }
     }
 
-    public void writeFeatureGeoJSON(final FitActivity fitActivity, final OutputStream out) throws FactoryException, IOException {
-        try {
+    /**
+     * The purpose of this method is to generate the schema definition for the GeoJSON.
+     * @return SimpleFeatureType defining the the feature properties.
+     */
+    public SimpleFeatureType getFeatureSchema() {
+        final SimpleFeatureTypeBuilder simpleFeatureType = new SimpleFeatureTypeBuilder();
+        simpleFeatureType.add("geom", LineString.class, DefaultGeographicCRS.WGS84);
+        simpleFeatureType.add("name", String.class);
+        simpleFeatureType.add("activityId", Double.class);
+        simpleFeatureType.setName("activity");
+        simpleFeatureType.add("activityName", String.class);
+        simpleFeatureType.add("sport", String.class);
+        simpleFeatureType.add("startTime", String.class);
+        simpleFeatureType.add("totalMeters", Double.class);
+        simpleFeatureType.add("totalSeconds", Double.class);
+        simpleFeatureType.add("minLat", Double.class);
+        simpleFeatureType.add("minLon", Double.class);
+        simpleFeatureType.add("maxLat", Double.class);
+        simpleFeatureType.add("maxLon", Double.class);
+        return simpleFeatureType.buildFeatureType();
+    }
 
-            final SimpleFeatureTypeBuilder tb=new SimpleFeatureTypeBuilder();
-            tb.add("geom",LineString.class, DefaultGeographicCRS.WGS84);
-            tb.add("name",String.class);
-            tb.add("activityId",Double.class);
-            tb.setName("activity");
-            tb.add("activityName", String.class);
-            tb.add("sport",String.class);
-            tb.add("startTime",String.class);
-            tb.add("totalMeters",Double.class);
-            tb.add("totalSeconds",Double.class);
-            final SimpleFeatureType schema=tb.buildFeatureType();
-            final SimpleFeatureBuilder fb=new SimpleFeatureBuilder(schema);
-            fb.set("activityId",fitActivity.getActivityId());
-            fb.set("sport",fitActivity.getSport());
-            fb.set("startTime",fitActivity.getStartTime());
-            fb.set("totalMeters",fitActivity.getTotalMeters());
-            fb.set("totalSeconds",fitActivity.getTotalSeconds());
-            final Coordinate[] polyline = fitActivity.getPolyline().toArray(new Coordinate[fitActivity.getPolyline().size()]);
-            final Geometry geometry = simplifyLineString(polyline);
-            fb.add(geometry);
-            final SimpleFeature feature=fb.buildFeature("0");
-            final FeatureJSON fj=new FeatureJSON();
-            fj.writeFeature(feature,out);
+    /**
+     * The purpose of this method is to build a SimpleFeature by assigning
+     * properties from the FitActivity to the properties defined in the SimpleFeatureType.
+     * @param fitActivity
+     * @return
+     */
+    public SimpleFeature buildSimpleFeature(final FitActivity fitActivity) {
+        final SimpleFeatureType featureSchema = getFeatureSchema();
+        final SimpleFeatureBuilder builder = new SimpleFeatureBuilder(featureSchema);
+        builder.set("activityId", fitActivity.getActivityId());
+        builder.set("sport", fitActivity.getSport());
+        builder.set("startTime", fitActivity.getStartTime());
+        builder.set("totalMeters", fitActivity.getTotalMeters());
+        builder.set("totalSeconds", fitActivity.getTotalSeconds());
+        final Coordinate[] polyline = fitActivity.getPolyline().toArray(new Coordinate[fitActivity.getPolyline().size()]);
+        final Geometry geometry = simplifyLineString(polyline);
+        builder.add(geometry);
+        final Coordinate[] boundingBox = generateBoundingBox(geometry);
+        builder.set("minLat", boundingBox[0].y);
+        builder.set("minLon", boundingBox[0].x);
+        builder.set("maxLat", boundingBox[1].y);
+        builder.set("maxLon", boundingBox[1].x);
+        return builder.buildFeature("0");
+    }
+
+    public void writeFeatureGeoJSON(final FitActivity fitActivity, final OutputStream out) throws FactoryException, IOException {
+        final SimpleFeature feature = buildSimpleFeature(fitActivity);
+        final FeatureJSON fj = new FeatureJSON();
+        try {
+            fj.writeFeature(feature, out);
         } finally {
             try {
                 out.close();
@@ -131,6 +156,41 @@ public class GarminFitService {
                 log.error(e);
             }
         }
+    }
+
+    /**
+     * The purpose of this method is to find the minimum/maximum lat/lon bounds.
+     *
+     * @param geometry - The geometry for which the southwest and northeast corners will be found.
+     * @return the minimum lat/lon and maximum lat/lon.
+     */
+    private Coordinate[] generateBoundingBox(final Geometry geometry) {
+        if (geometry == null || geometry.getCoordinates() == null || geometry.getCoordinates().length == 0) {
+            return new Coordinate[]{new Coordinate(0,0),new Coordinate(0,0)};
+        }
+        final Coordinate firstPoint = geometry.getCoordinates()[0];
+        double minLat = firstPoint.y;
+        double minLon = firstPoint.x;
+        double maxLat = firstPoint.y;
+        double maxLon = firstPoint.x;
+        for (final Coordinate coordinate : geometry.getCoordinates()) {
+            if (coordinate.x < minLon) {
+                minLon = coordinate.x;
+            }
+            if (coordinate.y < minLat) {
+                minLat = coordinate.y;
+            }
+            if (coordinate.x > maxLon) {
+                maxLon = coordinate.x;
+            }
+            if (coordinate.y > maxLat) {
+                maxLat = coordinate.y;
+            }
+        }
+        return new Coordinate[]{
+                new Coordinate(minLon, minLat),
+                new Coordinate(maxLon, maxLat)
+        };
     }
 
 }
